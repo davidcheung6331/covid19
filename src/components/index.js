ecd05a60e6cc7b53b16d819c3f8e7c99ad67162e
hkwma david <hkwma.david@gmail.com>
2022-10-13 14:36:41 +0800

    add commit

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,7 @@
 //take all the components and export it to the components folder.
 
-export { default as Cards} from './Cards/Cards'
-export { default as Charts} from './Charts/Charts'
-export { default as CountryPicker} from './CountryPicker/CountryPicker'
+export { default as Cards } from './Cards/Cards'
+export { default as Charts } from './Charts/Charts'
+export { default as CountryPicker } from './CountryPicker/CountryPicker'
+
+// only comment
